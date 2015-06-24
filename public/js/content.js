dde094e72f397a2bfe68289b1bd676418b5c1cd4
Tom Boutell <tom@punkave.com>
2015-06-24 12:08:14 -0400

    browser side error handling for forms, extensible, uses apos events, is not tested

--- public/js/content.js
+++ public/js/content.js
@@ -4,25 +4,38 @@ apos.widgetPlayers.forms = function($el) {
   $form.on('submit', function() {
     var action = $form.attr('action');
     var result = {};
+    var errors = [];
+
     $form.find('[data-forms-name]').each(function() {
       var $field = $(this);
       var key = $(this).attr('data-forms-name');
-      // checkboxes build arrays, everything else is simple
-      if ($field.attr('type') === 'checkbox') {
-        if (!_.has(result, key)) {
-          result[key] = [];
-        }
-        if ($field.prop('checked')) {
-          var val = $field.attr('value');
-          result[key].push(val);
-        }
-      } else {
+      apos.emit('sanitizeFormField', $field, key, result, errors);
+      if (!_.has(result, key)) {
+        // simple field
         result[key] = $field.val();
       }
     });
+
+    apos.emit('sanitizeForm', $form, result, errors);
+
+    if (errors.length) {
+      var $first;
+      _.each(errors, function(error) {
+        var $fieldset = $form.find('[data-forms-field-name="' + error.name + '"]');
+        $fieldset.addClass('apos-forms-error');
+        var $message = $('<div class="apos-forms-error-message"></div>');
+        $message.text(error.message);
+        $fieldset.append($message);
+        if (!$first) {
+          $first = $fieldset;
+        }
+      });
+      $first.scrollintoview();
+      return;
+    }
+
     $.jsonCall(action, result, function(data) {
       if (data.status !== 'ok') {
-        alert('An error occurred. Please try again later.');
         return;
       }
       $el.html(data.replacement);
@@ -30,3 +43,44 @@ apos.widgetPlayers.forms = function($el) {
     return false;
   });
 };
+
+// checkboxes build arrays
+apos.on('sanitizeFormField', function($field, key, result, errors) {
+  if ($field.attr('type') !== 'checkbox') {
+    return;
+  }
+  if (!_.has(result, key)) {
+    result[key] = [];
+  }
+  if ($field.prop('checked')) {
+    var val = $field.attr('value');
+    result[key].push(val);
+  }
+});
+
+// checkbox groups can have overall min and max
+apos.on('sanitizeForm', function($form, result, errors) {
+  $form.find('[data-forms-checkboxes]').each(function() {
+    var $field = $(this);
+    var min = $field.attr('data-forms-checkbox-min');
+    var max = $field.attr('data-forms-checkbox-max');
+    var name = $field.attr('data-forms-field-name')
+    if(min > 0 || max > 0) {
+     var checked = 0;
+     checked = result[name].length;
+
+     if((min && (checked < min)) || (max && (checked > max))) {
+       var message;
+       if (checked < min) {
+         message = 'Please select at least ' + min + ' checkboxes.';
+       } else {
+         message = 'Please select no more than ' + max + ' checkboxes.';
+       }
+       errors.push({
+         name: name,
+         message: message
+       });
+     }
+    }
+  });
+});
