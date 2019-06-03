24e652581f50b4012200963207bfddb473213630
Alex Bea <alex.bea@gmail.com>
2019-06-03 15:26:02 -0400

    Adds basic structure

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'apostrophe-forms',
+  moogBundle: {
+    directory: 'lib/modules',
+    modules: [
+      'apostrophe-forms-base-field'
+    ]
+  },
+  construct: function (self, options) {
+
+  }
+};
