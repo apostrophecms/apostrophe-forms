fbfba61767fc88a59d6be59f02ab2d94fdbbaccb
Alex Bea <alex.bea@gmail.com>
2019-08-08 08:50:00 -0500

    Removes notify method call

--- index.js
+++ index.js
@@ -367,12 +367,6 @@ module.exports = {
       } catch (err) {
         self.apos.utils.error('⚠️ apostrophe-forms submission email notification error: ', err);
 
-        // We don't want to throw an error simply because emails didn't send.
-        self.apos.notify(req, 'There was an error sending email notifications.', {
-          type: 'error',
-          dismiss: true
-        });
-
         return null;
       }
     });
